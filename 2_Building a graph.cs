648d842c9b5bdec94b1f59062412930c1c5db3ea
LaTrizz <56492293+LaTrizz@users.noreply.github.com>
2019-10-14 10:39:57 +0200

    Update 2_Building a graph.cs

--- 2_Building a graph.cs
+++ 2_Building a graph.cs
@@ -7,10 +7,17 @@ public class Graph : MonoBehaviour
   
     void Awake () 
     {
+        Vector3 scale = Vector3.one / 5f;
+        Vector3 position;
+        position.x = 0;
+        position.y = 0;
+    
         for(int i = 0; i < 10; i++)
         {
             Transform point = Instantiate (pointPrefab);
-            point.localPosition = Vector3.right;
+            position.x = ((i + 0.5f) / 5f - 1f);
+            point.localPosition = position;
+            point.localScale = scale;
         }
     }
 }
