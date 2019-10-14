cdb3cb5a395dee9c06ec792a94d1b6f37aacfc61
LaTrizz <56492293+LaTrizz@users.noreply.github.com>
2019-10-14 10:56:02 +0200

    Update 2_Building a graph.cs

--- 2_Building a graph.cs
+++ 2_Building a graph.cs
@@ -7,17 +7,22 @@ public class Graph : MonoBehaviour
   
     void Awake () 
     {
+        [Range (10, 100)]
+        public int resolution = 10;
+        
+        float step = 2f / resolution;
         Vector3 scale = Vector3.one / 5f;
         Vector3 position;
         position.x = 0;
-        position.y = 0;
     
-        for(int i = 0; i < 10; i++)
+        for(int i = 0; i < resolution; i++)
         {
             Transform point = Instantiate (pointPrefab);
-            position.x = ((i + 0.5f) / 5f - 1f);
+            position.x = ((i + 0.5f) * step - 1f);
+            position.y = position.x;
             point.localPosition = position;
             point.localScale = scale;
+            point.SetParent(transform);     // transform est 
         }
     }
 }
